b3ea6ddc86a7ce07a63a191f2903067a51587578
adrianox073 <adrianoxy@gmail.com>
2022-03-01 12:31:20 +0000

    capitulo 10 del curso

--- src/main/java/com/personal/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/personal/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,12 +1,17 @@
 package com.personal.springboot.reactor.app;
 
+import com.personal.springboot.reactor.app.model.Usuario;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 import reactor.core.publisher.Flux;
+
+import java.util.Locale;
 
 @SpringBootApplication
 @Slf4j
@@ -17,27 +22,23 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(String... args) {
 
-		Flux<String> nombres = Flux.just("A", "C", "D", "D1", "E").doOnNext(
-
+		Flux<Usuario> usuarios = Flux.just("A", "C", "D", "D1", "E", "")
+				.map(u -> new Usuario(u.toUpperCase(), null))
+				.doOnNext(
 						elemento -> {
-							if (elemento.isEmpty()) {
+							if (elemento.getNombre().isEmpty()) {
 								throw new RuntimeException("Elemento vacio");
-							} else
-							{
-
-								System.out.println(elemento); }
+							}
+							System.out.println(elemento.getNombre());
 						}
-		);
+		).map(usuario -> {
+			usuario.setNombre(usuario.getNombre().toLowerCase());
+			return usuario;
+		});
 
-		nombres.subscribe(e -> log.info(e), err -> log.error(err.getMessage()), new Runnable () {
-
-					@Override
-					public void run() {
-                           log.info("Ha finalizado ");
-					}
-				}
+		usuarios.subscribe(e -> log.info(e.toString()), err -> log.error(err.getMessage()), () -> log.info("Ha finalizado ")
 		);
 	}
 }
---
+++ src/main/java/com/personal/springboot/reactor/app/model/Usuario.java
@@ -0,0 +1,13 @@
+package com.personal.springboot.reactor.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+    private String nombre;
+    private String apellido;
+}
